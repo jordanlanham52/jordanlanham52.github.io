169fd22976514a557080d58f3153c52bcd9c96d1
jordanlanham52 <jordanlanham52@gmail.com>
2025-02-20 17:42:43 -0700

    web base

---
+++ scripts.js
@@ -0,0 +1,13 @@
+const introText = "Welcome to my cyber domain...\n> Initializing...\n> Access Granted!";
+let i = 0;
+const speed = 50; 
+
+function typeEffect() {
+    if (i < introText.length) {
+        document.getElementById("intro-text").innerHTML += introText.charAt(i);
+        i++;
+        setTimeout(typeEffect, speed);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", typeEffect);
